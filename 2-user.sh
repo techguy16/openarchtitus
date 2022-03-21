ee393d877f5ecd153cc2a40a2cfb5b1d78ed0893
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-03-22 07:54:45 +1300

    Update 2-user.sh

--- 2-user.sh
+++ 2-user.sh
@@ -15,7 +15,7 @@ echo -ne "
 Installing AUR Softwares
 "
 # You can solve users running this script as root with this and then doing the same for the next for statement. However I will leave this up to you.
-source $HOME/ArchTitus/setup.conf
+source $HOME/openarchtitus/setup.conf
 
 cd ~
 git clone "https://aur.archlinux.org/yay.git"
@@ -27,10 +27,10 @@ git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 ln -s "~/zsh/.zshrc" ~/.zshrc
 
-yay -S --noconfirm --needed - < ~/ArchTitus/pkg-files/aur-pkgs.txt
+yay -S --noconfirm --needed - < ~/openarchtitus/pkg-files/aur-pkgs.txt
 
 export PATH=$PATH:~/.local/bin
-cp -r ~/ArchTitus/dotfiles/* ~/.config/
+cp -r ~/openarchtitus/dotfiles/* ~/.config/
 echo -ne "
 -------------------------------------------------------------------------
                     SYSTEM READY FOR 3-post-setup.sh

155550f196910a5636a4f73d6affd37af507ad25
techguy16 <88870951+techguy16@users.noreply.github.com>
2022-10-30 10:44:42 +1300

    @treatmeniceplz - Changed the text that displays the scripts location
    Pull Request

--- openarchtitus.sh
+++ openarchtitus.sh
@@ -14,16 +14,16 @@ echo -ne "
 -------------------------------------------------------------------------
                     Automated Arch Linux Installer
 -------------------------------------------------------------------------
-             Scripts are in directory named OpenArchTitus
+   Scripts are in directory named scripts (in OpenArchTitus directory)
 "
     ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
       source $CONFIGS_DIR/setup.conf
     ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
+    ( arch-chroot /mnt $HOME/openarchtitus/scripts/1-setup.sh )|& tee 1-setup.log
     if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
+      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/openarchtitus/scripts/2-user.sh )|& tee 2-user.log
     fi
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
+    ( arch-chroot /mnt $HOME/openarchtitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
     cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
